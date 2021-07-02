0c412914543981532534259c98dcc1851bc73ea9
Ali Pourkhesalian <pourkhesalian@gmail.com>
2021-07-02 13:22:51 +1000

    new avg

---
+++ avg_analysis.R
@@ -0,0 +1,13 @@
+#new graphs from the averaged data
+
+
+avgdata <- read.csv('avgDriftForR.csv')
+str(avgdata)
+ggplot(data= avgdata, aes(x= vg, y= kgdriftpabs, color= device))+
+  geom_point(aes(size= fliq, shape= packing))+
+  ylim(c(0,5))+ 
+  geom_smooth(method = 'lm')+
+  theme_bw()+
+  labs(title = 'Solvent loss VS air velocity for different devices and defferent flowrates', 
+       x='Air velocity (m/s)', 
+       y= 'kg Solvent loss per ton of CO2 absorbed (kg/ton)')
